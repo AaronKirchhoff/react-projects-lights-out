287a03cbf885810833d8f60a671fcb924459ca62
Aaron Kirchhoff <askirchhoff@gmail.com>
2021-08-03 08:37:11 -0700

    Final Commit 8.3.21

--- src/Board.js
+++ src/Board.js
@@ -42,21 +42,30 @@ class Board extends Component {
       }
     }
 
+    // increasing or decreasing the x,y coordinate calls flipcell function, this is flipping a cell, and all 4 around it too
     flipCell(y, x);
     flipCell(y, x - 1);
     flipCell(y, x + 1);
+    flipCell(y - 1, x);
+    flipCell(y + 1, x);
 
-    let hasWon = false;
-    this.setState({ board, hasWon });
+    // after you flipcells, check everytime if every cell if off/ false this will tell us if we have won. checking every cell, in every row should be false
+    let hasWon = board.every(row => row.every(cell => !cell));
+    this.setState({ board: board, hasWon: hasWon });
   }
 
-  render() {
+  makeTable() {
     let tblBoard = [];
     for (let y = 0; y < this.props.nrows; y++){
       let row = [];
       for (let x = 0; x < this.props.ncols; x++){
         let coord = `${y}-${x}`
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]} flipCellsAroundMe={() => this.flipCellsAround(coord)}/>);
+        row.push(
+          <Cell 
+            key={coord} 
+            isLit={this.state.board[y][x]} flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          />
+        );
       }
       tblBoard.push(<tr key={y}>{row}</tr>);
     }
@@ -64,7 +73,28 @@ class Board extends Component {
       <table className="Board">
         <tbody>{tblBoard}</tbody>
       </table>
-    )
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.hasWon ? (
+          <div className='winner'>
+            <span className='neon-orange'>YOU</span>
+            <span className='neon-blue'>WIN!</span>
+          </div>
+        ) : (
+          <div>
+            <div className='Board-title'>
+              <div className='neon-orange'>Lights</div>
+              <div className='neon-blue'>Out</div>
+            </div>
+            {this.makeTable()}
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
